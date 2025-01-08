ea9830ec069159938150da600b42813f62f8c627
Muhammad Early Maulana <earlymaulana16@gmail.com>
2025-01-08 17:10:57 +0700

    ketinggalan

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -4,17 +4,44 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
-    {
-        return $next($request);
+    public function handle(Request $request, Closure $next)
+{
+    /** @var User $user */
+    $user = Auth::user();
+
+    if (!$user) {
+        return redirect()->route('login')->with('error', 'You must be logged in.');
     }
+
+   
+    if ($user->hasRole('Admin') && !$request->routeIs('dashboard.admin')) {
+        return redirect()->route('dashboard.admin');
+    } elseif ($user->hasRole('Kasir') && !$request->routeIs('dashboard.kasir')) {
+        return redirect()->route('dashboard.kasir');
+    } elseif ($user->hasRole('Supervisor') && !$request->routeIs('dashboard.supervisor')) {
+        return redirect()->route('dashboard.supervisor');
+    } elseif ($user->hasRole('Manajer Toko') && !$request->routeIs('dashboard.manajer')) {
+        return redirect()->route('dashboard.manajer');
+    } elseif ($user->hasRole('Gudang') && !$request->routeIs('dashboard.gudang')) {
+        return redirect()->route('dashboard.gudang');
+    }
+
+    if (!$request->routeIs('dashboard')) {
+        return redirect()->route('dashboard')->with('error', 'Role not recognized.');
+    }
+
+    return $next($request);
+}
+
 }
