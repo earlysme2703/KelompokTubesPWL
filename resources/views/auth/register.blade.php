b37e96cb269da8c01e62fdca0c11f54d340bf0ed
M. Early Maulana <142871177+earlysme2703@users.noreply.github.com>
2025-01-10 16:45:46 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Platform As A Service</title>
+    <title>Jayusman Minimarket</title>
     <!-- Tailwind CSS dan Flowbite CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
